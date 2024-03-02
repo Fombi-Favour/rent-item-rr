5b7ab581098ff82f94bf8eff0cc00ba2e4625b3d
Fombi Favour <fombifavour@gmail.com>
2024-03-02 10:23:19 +0100

    Update residency.rb

--- backend/app/models/residency.rb
+++ backend/app/models/residency.rb
@@ -1,6 +1,7 @@
 class Residency < ApplicationRecord
   belongs_to :user
 
+  has_many :bookings, foreign_key: 'residency_id', dependent: :destroy
   has_many :reviews, foreign_key: 'user_id', dependent: :destroy
 
   validates :name, presence: true
