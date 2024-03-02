2eeaa8bd01a487763d1ea98d17bef034ebd5a030
Fombi Favour <fombifavour@gmail.com>
2024-03-02 09:34:48 +0100

    Update 20240302001432_create_residencies.rb

--- backend/db/migrate/20240302001432_create_residencies.rb
+++ backend/db/migrate/20240302001432_create_residencies.rb
@@ -1,6 +1,7 @@
 class CreateResidencies < ActiveRecord::Migration[7.1]
   def change
     create_table :residencies do |t|
+      t.string :name
       t.string :location
       t.string :category
       t.text :description
