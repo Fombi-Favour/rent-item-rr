189543812cfc50ad05453d91c00101ae1d80b172
Fombi Favour <fombifavour@gmail.com>
2024-03-13 01:37:53 +0100

    Update routes.rb

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+
+  post 'sign_up', to: 'api/v1/users#sign_up'
+  post 'login', to: 'api/v1/users#login'
+
   namespace :api do
     namespace :v1 do
       resources :users do
