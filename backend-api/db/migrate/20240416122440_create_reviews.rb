262a0df762a21f3112746ca71c095de516de759c
Fombi Favour <fombifavour@gmail.com>
2024-04-16 13:44:47 +0100

    create review model

---
+++ backend-api/db/migrate/20240416122440_create_reviews.rb
@@ -0,0 +1,12 @@
+class CreateReviews < ActiveRecord::Migration[7.1]
+  def change
+    create_table :reviews do |t|
+      t.decimal :rating
+      t.string :comment_text
+      t.references :user, foreign_key: true
+      t.references :residency, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
