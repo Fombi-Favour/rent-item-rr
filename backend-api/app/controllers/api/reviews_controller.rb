938e2148af80470896c8ddb01e4a0f70a3cd2079
Fombi Favour <fombifavour@gmail.com>
2024-04-23 00:57:38 +0100

    Update reviews_controller.rb

--- backend-api/app/controllers/api/reviews_controller.rb
+++ backend-api/app/controllers/api/reviews_controller.rb
@@ -1,9 +1,28 @@
 class Api::ReviewsController < ApplicationController
   before_action :authenticate_token!
 
+  def index
+    @residency = Residency.find(params[:residency_id])
+    @reviews = @residency.reviews.includes(:residency)
+
+    render json: @reviews.map { |review| review.attributes.except('created_at', 'updated_at') }
+  end
+
+  def create
+    residency = Residency.find(params[:residency_id])
+
+    @review = current_user.reviews.build(reviews_params)
+
+    if @review.save
+      render json: @review
+    else
+      render json: @review.errors, status: :unprocessable_entity
+    end
+  end
+
   private
 
   def reviews_params
-    params.permit(:rating, :comment_text)
+    params.permit(:rating, :comment_text, :residency_id)
   end
 end
