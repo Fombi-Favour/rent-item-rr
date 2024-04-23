66f41d604b9d18ac6032cb50a2b2f066a272bde5
Fombi Favour <fombifavour@gmail.com>
2024-04-23 16:26:37 +0100

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,28 @@
-import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import Home from './containers/Home';
+import Login from './containers/Login';
+import Register from './containers/Register';
+import Dashboard from './containers/Dashboard';
+import Layout from './components/Layout';
+
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Welcome to this page</h1>
-    </div>
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="login" element={<Login />} />
+        <Route path="sign-up" element={<Register />} />
+        <Route path="residency" element={<Layout />}>
+          <Route index element={<Dashboard />} />
+        </Route>
+      </Routes>
+      <ToastContainer />
+    </>
   );
 }
 
