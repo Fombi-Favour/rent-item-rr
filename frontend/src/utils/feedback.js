b61ff84b0fcd842e087e4565b6b12c017a6e1038
Fombi Favour <fombifavour@gmail.com>
2024-04-24 21:39:03 +0100

    Create feedback.js

---
+++ frontend/src/utils/feedback.js
@@ -0,0 +1,60 @@
+const feedback = [
+  {
+    id: 1,
+    userName: 'Bobby Won',
+    image: 'https://images.pexels.com/photos/10276340/pexels-photo-10276340.jpeg?auto=compress&cs=tinysrgb&w=600',
+    text: 'HoliStay is an excellent platform. I was able to find an available place to spend my vacation. The services are the best.',
+    rating: 4.0,
+  },
+  {
+    id: 2,
+    userName: 'Kate Swome',
+    image: 'https://images.pexels.com/photos/3992656/pexels-photo-3992656.png?auto=compress&cs=tinysrgb&w=600',
+    text: 'HoliStay is an excellent platform. I was able to find an available place to spend my vacation. The services are the best.',
+    rating: 4.5,
+  },
+  {
+    id: 3,
+    userName: 'Morgan Abrams',
+    image: 'https://images.pexels.com/photos/10276340/pexels-photo-10276340.jpeg?auto=compress&cs=tinysrgb&w=600',
+    text: 'HoliStay is an excellent platform. I was able to find an available place to spend my vacation. The services are the best.',
+    rating: 4.0,
+  },
+  {
+    id: 4,
+    userName: 'David Anderson',
+    image: 'https://images.pexels.com/photos/10276340/pexels-photo-10276340.jpeg?auto=compress&cs=tinysrgb&w=600',
+    text: 'HoliStay is an excellent platform. I was able to find an available place to spend my vacation. The services are the best.',
+    rating: 4.0,
+  },
+  {
+    id: 5,
+    userName: 'Sarah Miller',
+    image: 'https://images.pexels.com/photos/3992656/pexels-photo-3992656.png?auto=compress&cs=tinysrgb&w=600',
+    text: 'HoliStay is an excellent platform. I was able to find an available place to spend my vacation. The services are the best.',
+    rating: 4.5,
+  },
+  {
+    id: 6,
+    userName: 'Camila Rodriguez',
+    image: 'https://images.pexels.com/photos/3992656/pexels-photo-3992656.png?auto=compress&cs=tinysrgb&w=600',
+    text: 'HoliStay is an excellent platform. I was able to find an available place to spend my vacation. The services are the best.',
+    rating: 4.5,
+  },
+  {
+    id: 7,
+    userName: 'Ethan Kim',
+    image: 'https://images.pexels.com/photos/10276340/pexels-photo-10276340.jpeg?auto=compress&cs=tinysrgb&w=600',
+    text: 'HoliStay is an excellent platform. I was able to find an available place to spend my vacation. The services are the best.',
+    rating: 4.0,
+  },
+  {
+    id: 6,
+    userName: 'Maya Lee',
+    image: 'https://images.pexels.com/photos/3992656/pexels-photo-3992656.png?auto=compress&cs=tinysrgb&w=600',
+    text: 'HoliStay is an excellent platform. I was able to find an available place to spend my vacation. The services are the best.',
+    rating: 4.5,
+  },
+];
+
+export default feedback;
