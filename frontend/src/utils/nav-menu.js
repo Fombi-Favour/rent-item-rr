c11a404f59ca01069a08d565b801e79ebfb7dace
Fombi Favour <fombifavour@gmail.com>
2024-04-25 16:08:35 +0100

    Create nav-menu.js

---
+++ frontend/src/utils/nav-menu.js
@@ -0,0 +1,35 @@
+const navMenu = [
+  {
+    id: 1,
+    path: '/residency',
+    name: 'Residencies',
+  },
+  {
+    id: 2,
+    path: '/reservation/new',
+    name: 'Book a Reservation',
+  },
+  {
+    id: 3,
+    path: '/reservation',
+    name: 'Reservations',
+  },
+
+  {
+    id: 4,
+    path: '/residency/new',
+    name: 'Add Residency',
+  },
+  {
+    id: 5,
+    path: '/residency/delete',
+    name: 'Delete residency',
+  },
+  {
+    id: 6,
+    path: '/feedback',
+    name: 'Feedback',
+  },
+];
+
+export default navMenu;
