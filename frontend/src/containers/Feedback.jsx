4d07f4e5b5719af09049174525f3ddb04fb8c05d
Fombi Favour <77404317+Fombi-Favour@users.noreply.github.com>
2024-11-28 17:03:34 +0100

    Update Feedback.jsx

--- frontend/src/containers/Feedback.jsx
+++ frontend/src/containers/Feedback.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
 
 const Feedback = () => (
-  <div>Feedback</div>
+  <section className="bg-img-feedback w-full h-screen bg-cover md:bg-center">
+    <div className="w-full h-full bg-white/40 flex flex-col items-center justify-center">
+      feedback
+    </div>
+  </section>
 );
 
 export default Feedback;
