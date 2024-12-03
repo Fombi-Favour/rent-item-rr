114128a9e08f9d1459fb6d9c76c6a8642a6a2b24
Fombi Favour <77404317+Fombi-Favour@users.noreply.github.com>
2024-12-03 21:15:39 +0100

    Create Ratings.jsx

---
+++ frontend/src/components/Ratings.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { FaStar } from 'react-icons/fa';
+
+const Ratings = () => (
+  <div className="bg-white w-full py-4 px-6 md:rounded-r-2xl">
+    <FaStar className="text-xl text-yellow-400" />
+  </div>
+);
+
+export default Ratings;
